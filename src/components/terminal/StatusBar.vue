645e18ed6d21dff022c6962153afd4b40cb5de53
André <andre@brayn.io>
2026-09-13 11:48:00 +0200

    refactor: drop the UTF-8 marker from the status bar
    It imitated the encoding indicator of an editor status bar and told a visitor
    nothing. The two neighbours stay, they are at least hints at what the page can
    do: ⌘K opens the palette, the arrow sequence is the easter egg.

--- src/components/terminal/StatusBar.vue
+++ src/components/terminal/StatusBar.vue
@@ -5,7 +5,6 @@
     <span class="term-status-right">
       <RouterLink to="/legal#impressum">{{ rd.footerImprint }}</RouterLink>
       <RouterLink to="/legal#datenschutz">{{ rd.footerPrivacy }}</RouterLink>
-      <span class="term-status-desktop">UTF-8</span>
       <span class="term-status-desktop">⌘K</span>
       <span class="term-status-desktop" title="Konami">{{ rd.konamiHint }}</span>
     </span>
